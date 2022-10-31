4fd2d95276e19d7a5b472d8ffa5b2bf8e224ed6b
Nikita Chelyshev <60578593+Rocky-Webster@users.noreply.github.com>
2022-10-31 19:12:16 +0300

    TelegramBot.py

---
+++ botRun.bat
@@ -0,0 +1,12 @@
+@echo off
+
+call D:\Projects\TelegramBot\venv\Scripts\activate
+
+cd D:\Projects\TelegramBot
+
+set TOKEN = '5429668333:AAFZ1nZEThvZ2Nn9OCBUtzT2Gy2qx9KPneU'
+
+py telegramBot.py TOKEN
+
+
+pause
